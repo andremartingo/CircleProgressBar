3c8bab3fb0ae6366f750f74e9568045405168784
André Martingo <andre.martingo@gmail.com>
2019-04-13 11:53:25 +0100

    Initial Commit

---
+++ circleprogressbar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  circleprogressbar
+//
+//  Created by André Martingo on 13/04/2019.
+//  Copyright © 2019 André Martingo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
